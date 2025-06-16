852e43a1fbe1e80049fb6a5f6190f6d109182856
weeperzzang <weeperzzang@naver.com>
2025-06-16 09:58:44 +0900

    vvvv

---
+++ Editor/PlayerPrefsEditor2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public class PlayerPrefsEditor2 : EditorWindow
+    {
+        private const string Title = "PlayerPrefs2";
+        
+        [MenuItem("aaaaa/PlayerPrefs2")]
+        public static void ShowWindow()
+        {
+            GetWindow<PlayerPrefsEditor2>(Title);
+        }
+    }
+}
